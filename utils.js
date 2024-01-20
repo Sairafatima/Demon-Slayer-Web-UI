61af0893983456122b40635100afe8ccb4dda105
sairafatima <saira030854@gmail.com>
2024-01-20 13:28:48 +0500

    update

---
+++ utils.js
@@ -0,0 +1,5 @@
+const herosMap={tanjiro:{
+    rank:"1",
+    intro:"Tanjiro is very kind by nature and has been described by others as having very gentle eyes and a compassionate personamado",
+    name:"Tanjiro Kamado"
+}}
