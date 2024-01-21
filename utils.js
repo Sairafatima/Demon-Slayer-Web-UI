a5f18e9a2b2ff73be3d54c25c712f27134a4ed6b
sairafatima <saira030854@gmail.com>
2024-01-21 14:13:01 +0500

    updates

--- utils.js
+++ utils.js
@@ -5,3 +5,30 @@ const tanjiro = {
     name: "Tanjiro Kamado",
   
 };
+
+function goToHeroSection(id){
+    // document is html page
+    // getElementById function returns the elemtn which has heros id
+    // scrollIntoView function scroll to the element on which it is called on
+    // behavior:"smooth" shows the scroll transition to the element
+    // this function can be optimized by using id from funtion input/argument
+    document.getElementById("heros").scrollIntoView({behavior:"smooth"})
+}
+function goToVilliansSection(){
+     // document is html page
+    // getElementById function returns the elemtn which has heros id
+    // scrollIntoView function scroll to the element on which it is called on
+    // behavior:"smooth" shows the scroll transition to the element
+    document.getElementById("villians").scrollIntoView({behavior:"smooth"})
+}
+function goToContactSection(){
+     // document is html page
+    // getElementById function returns the elemtn which has heros id
+    // scrollIntoView function scroll to the element on which it is called on
+    // behavior:"smooth" shows the scroll transition to the element
+    document.getElementById("contact").scrollIntoView({behavior:"smooth"})
+}
+function viewGallery(imageNumber){
+document.getElementById("gallery").style.display="block"
+
+}
